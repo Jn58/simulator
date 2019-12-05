f913d8975a928fd99937d10d59543d7b72400ee5
Unknown <holyavarice@gmail.com>
2019-12-05 23:11:23 +0900

    check function

--- src/queue_algorithm.cpp
+++ src/queue_algorithm.cpp
@@ -3,6 +3,13 @@
 
 
 namespace ClusterSimulator {
+	bool GeneAlgorithm::check(std::vector<std::shared_ptr<Job>>& jobs)
+	{
+		if(jobs.size() < length ) return false;
+		if(jobs[jobs.size() - length]->state != JobState::PEND  ) return false;
+
+		return true;
+	}
 	void GeneAlgorithm::run(std::vector<std::shared_ptr<Job>>& jobs) {
 		assert(check());
 		exec();
