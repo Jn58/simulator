3e51a51629d2414bddcbe7f728def79b7b104e97
Unknown <holyavarice@gmail.com>
2019-12-05 18:14:04 +0900

    make it compilable

--- src/queue_algorithm.cpp
+++ src/queue_algorithm.cpp
@@ -3,10 +3,10 @@
 
 
 namespace ClusterSimulator {
-	void GeneAlgorithm::run(std::vector<std::shared_ptr<Job>>& jobs)	{
+	void GeneAlgorithm::run(std::vector<std::shared_ptr<Job>>& jobs) {
 		assert(check());
 		exec();
 		enqueJobs(jobs);
-		step();		
+		step();
 	}
 }
