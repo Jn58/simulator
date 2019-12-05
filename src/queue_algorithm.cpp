0ae283e7a4602a1d891f99d0adc7414bba3aecea
Unknown <holyavarice@gmail.com>
2019-12-05 17:19:50 +0900

    run code

--- src/queue_algorithm.cpp
+++ src/queue_algorithm.cpp
@@ -1,4 +1,12 @@
 #include "queue_algorithm.h"
+#include <cassert>
+
 
 namespace ClusterSimulator {
+	void GeneAlgorithm::run(std::vector<std::shared_ptr<Job>>& jobs)	{
+		assert(check());
+		exec();
+		enqueJobs(jobs);
+		step();		
+	}
 }
